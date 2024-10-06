423d02ca009416e5fbcafdc4d1b9d993cb0fca1a
Pablo Friedmann <pabloifm91@gmail.com>
2024-10-06 21:33:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,68 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let inicio = "Un ";
+  let adjetivos = [
+    "horrible ",
+    "gigantesco ",
+    "loco ",
+    "despistado ",
+    "rápido ",
+    "oloroso ",
+    "brillante ",
+    "perezoso ",
+    "anciano "
+  ];
+  let sustantivos = [
+    "señor ",
+    "gatito ",
+    "perro ",
+    "vendedor de callcenter ",
+    "conductor ",
+    "comediante ",
+    "erizo "
+  ];
+  let acciones = [
+    "se llevó mi ",
+    "tiró mi ",
+    "gritó a mi ",
+    "pateó mi ",
+    "robó mi ",
+    "quemó mi ",
+    "mordió mi ",
+    "golpeó mi "
+  ];
+  let posesiones = [
+    "móvil ",
+    "coche ",
+    "reloj ",
+    "teléfono ",
+    "camisa ",
+    "coche ",
+    "mechero ",
+    "bocadillo "
+  ];
+  let lugares = [
+    "en Murcia",
+    "en el baño de mi curro",
+    "en el Río Turia",
+    "frente a la oficina",
+    "en mi habitación",
+    "en la estación de autobuses",
+    "al lado del baño"
+git add .  ];
+
+  let rdm1 = Math.floor(Math.random() * adjetivos.length);
+  let rdm2 = Math.floor(Math.random() * sustantivos.length);
+  let rdm3 = Math.floor(Math.random() * acciones.length);
+  let rdm4 = Math.floor(Math.random() * posesiones.length);
+  let rdm5 = Math.floor(Math.random() * lugares.length);
+
+  let excusa =
+    inicio +
+    adjetivos[rdm1] +
+    sustantivos[rdm2] +
+    acciones[rdm3] +
+    posesiones[rdm4] +
+    lugares[rdm5];
+
+  document.querySelector("#excusa").innerHTML = excusa;
 };
